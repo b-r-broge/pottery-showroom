520a80852f5f3085f3d2b98c6c4c084219eea383
Brad Broge <brbroge@gmail.com>
2023-05-29 11:05:44 -0400

    playing around with CSS stylings

--- src/components/DisplayTile.tsx
+++ src/components/DisplayTile.tsx
@@ -1,17 +1,16 @@
 import Image from 'next/image';
+import styles from '@/styles/Home.module.css';
 
 import { item } from '@/types';
 
-export default function DisplayTile( { piece, key}: {piece: item, key: string} ) {
+export default function DisplayTile( { piece }: {piece: item} ) {
   const src = `/${piece.photoRoot}/1.jpg`;
   const gif = `/${piece.photoRoot}/exportGif.gif`;
-  const alt = `A piece of pottery that is a ${piece.type}, that is primarily glazed in ${piece.primaryColor}${piece.blend ? ` with a ${piece.blend} of ${piece.secondaryColor}.` : '.'}`
-
+  const alt = `A piece of pottery that is a ${piece.type}, that is primarily glazed in ${piece.primaryColor}${piece.blend ? ` with a ${piece.blend} of ${piece.secondaryColor}.` : '.'}`;
 
   return (
-    <div className='piece-tile'>
-      <span className='piece-title'>{piece.id}</span>
-      <br/>
+    <div className={styles.card}>
+      <h2>{piece.id}</h2>
       <Image
         className='piece-pic'
         src={src}
